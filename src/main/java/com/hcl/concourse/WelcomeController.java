38ad6cc6a2167f316ae2c65606678a1b68539492
bikash.k <bikash.k@LP-5CD6495TH7.HCLC.CORP.HCL.IN>
2018-10-23 12:07:44 +0530

    comments to test

--- src/main/java/com/hcl/concourse/WelcomeController.java
+++ src/main/java/com/hcl/concourse/WelcomeController.java
@@ -2,12 +2,16 @@ package com.hcl.concourse;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * 
+ * @author bikash.k
+ *
+ */
 @RestController
 public class WelcomeController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "hello World of Spring boot 2.0";
+        return "hello World of Spring boot 2.0.1";
     }
 }
